6957aa204e09a08270f842da1a71401f6e870773
erdaoya <zz.thoreau@Gmail.com>
2017-01-12 22:36:24 +0800

    add Circuit Breaker

--- cloud-service-client/src/main/java/com/erdaoya/springcloud/client/service/UserFeignClient.java
+++ cloud-service-client/src/main/java/com/erdaoya/springcloud/client/service/UserFeignClient.java
@@ -1,5 +1,6 @@
 package com.erdaoya.springcloud.client.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *@author erdaoya
  *@data 2016/12/25 21:43
  */
-@FeignClient("user-service")
+@FeignClient(name = "user-service", fallback = UserFeignClient.HystrixClientFallback.class)
 public interface UserFeignClient {
 	@RequestMapping(method = RequestMethod.GET)
     Object getUser(@RequestParam( value= "id" ) String id);
+
+	static class HystrixClientFallback implements UserFeignClient{
+
+        @Override
+        public Object getUser(@RequestParam(value = "id") String id) {
+            return "service Circuit Breaker";
+        }
+    }
+
 }
