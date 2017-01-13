d6c8115a938d372942db70f535ba9e0929de9da7
zhaozhou <zhaozhou@gomeplus.com>
2017-01-13 11:43:55 +0800

    delete Repeat class

--- cloud-service-client/src/main/java/com/erdaoya/springcloud/client/web/UserController.java
+++ cloud-service-client/src/main/java/com/erdaoya/springcloud/client/web/UserController.java
@@ -1,6 +1,7 @@
 package com.erdaoya.springcloud.client.web;
 
 import com.erdaoya.springcloud.client.client.UserFeignClient;
+import com.erdaoya.springcloud.client.client.UserRibbonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,14 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 1.0
  */
 @RestController
-@RequestMapping("/feign/user")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserFeignClient userClient;
+    private UserRibbonClient ribbonClient;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public Object getUser(String id) {
-        return userClient.getUser(id);
+    @Autowired
+    private UserFeignClient feignClient;
+
+    @RequestMapping(value = "/feign", method = RequestMethod.GET)
+    public Object getUserByFeign(String id) {
+        return feignClient.getUser(id);
     }
+    @RequestMapping(value = "/ribbon", method = RequestMethod.GET)
+    public Object getUserByRibbon(String id) {
+        return ribbonClient.getUser(id);
+    }
+
 }
