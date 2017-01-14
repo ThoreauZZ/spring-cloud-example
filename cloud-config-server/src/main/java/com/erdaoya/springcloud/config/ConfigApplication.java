24c64f19cecf3f5871202ac4c5d4e5853f4e48bd
erdaoya <zz.thoreau@Gmail.com>
2017-01-14 16:30:55 +0800

    config server registry to eureka

--- cloud-config-server/src/main/java/com/erdaoya/springcloud/config/ConfigApplication.java
+++ cloud-config-server/src/main/java/com/erdaoya/springcloud/config/ConfigApplication.java
@@ -2,6 +2,7 @@ package com.erdaoya.springcloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigApplication {
     public static void main(String[] args) {
         long starTime = System.currentTimeMillis();
