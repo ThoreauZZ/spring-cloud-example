1f6b94ea547cb8cc06a3265077e5ce347bfc98da
erdaoya <zz.thoreau@Gmail.com>
2017-01-14 11:11:05 +0800

    EnableZipkinStreamServer

--- cloud-zipkin-ui/src/main/java/com/erdaoya/springcloud/zipkin/ZipkinServerApplication.java
+++ cloud-zipkin-ui/src/main/java/com/erdaoya/springcloud/zipkin/ZipkinServerApplication.java
@@ -3,6 +3,7 @@ package com.erdaoya.springcloud.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 /**
@@ -12,7 +13,7 @@ import zipkin.server.EnableZipkinServer;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinServerApplication {
     public static void main(String[] args) {
         long starTime = System.currentTimeMillis();
