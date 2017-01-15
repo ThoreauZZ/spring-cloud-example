691bb4c5007f81f9c987a714fc594006462ad93e
erdaoya <zhaozhou2010jz@163.com>
2017-01-15 23:10:25 +0800

    add new module admin

---
+++ cloud-admin-ui/src/main/java/com/erdaoya/springcloud/admin/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.erdaoya.springcloud.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        long starTime = System.currentTimeMillis();
+        SpringApplication.run(AdminApplication.class, args);
+        long endTime = System.currentTimeMillis();
+        long time = endTime - starTime;
+        System.out.println("\nStart Time: " + time / 1000 + " s");
+        System.out.println("...............................................................");
+        System.out.println("..................Service starts successfully..................");
+        System.out.println("...............................................................");
+
+    }
+}
