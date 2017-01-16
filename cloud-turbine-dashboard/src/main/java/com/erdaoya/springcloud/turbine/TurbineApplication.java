c2bf3139df1dac3d5b4ceb15edf7dbf942a3ced2
zhaozhou <zhaozhou@gomeplus.com>
2017-01-16 14:28:44 +0800

    chang name

---
+++ cloud-turbine-dashboard/src/main/java/com/erdaoya/springcloud/turbine/TurbineApplication.java
@@ -0,0 +1,29 @@
+package com.erdaoya.springcloud.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 2017/1/13
+ * @author  erdaoya
+ * @since   1.0
+ */
+@SpringBootApplication
+@EnableTurbine
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class TurbineApplication {
+    public static void main(String[] args) {
+        long starTime = System.currentTimeMillis();
+        SpringApplication.run(TurbineApplication.class, args);
+        long endTime = System.currentTimeMillis();
+        long time = endTime - starTime;
+        System.out.println("\nStart Time: " + time / 1000 + " s");
+        System.out.println("...............................................................");
+        System.out.println("..................Service starts successfully..................");
+        System.out.println("...............................................................");
+    }
+}
