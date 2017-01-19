a17c6437a2350ac9f1985721e6012f35e952cda3
zhaozhou <zhaozhou@gomeplus.com>
2017-01-19 12:14:17 +0800

    update config

--- cloud-service-ext/src/main/java/com/erdaoya/springcloud/client/client/UserFeignClient.java
+++ cloud-service-ext/src/main/java/com/erdaoya/springcloud/client/client/UserFeignClient.java
@@ -1,42 +1,20 @@
 package com.erdaoya.springcloud.client.client;
 
-import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * 2016/12/25 21:43
- *
  * @author erdaoya
  */
-@FeignClient(name = "user-service", fallback = HystrixClientFallback.class)
+@FeignClient(value = "user-service")
 public interface UserFeignClient {
+
 
     @RequestMapping(method = RequestMethod.GET)
     Object getUser(@RequestParam(value = "id") String id);
 
 
-}
-
-@Component
-class HystrixClientFallback implements FallbackFactory<UserFeignClient> {
-
-
-    @Override
-    public UserFeignClient create(Throwable throwable) {
-        return new UserFeignClient() {
-            public Object getUser(String id) {
-                Map<String, String> map = new HashMap<String, String>();
-                map.put("mesages", "Hystrix Fallback");
-                return map;
-            }
-        };
-
-    }
 }
