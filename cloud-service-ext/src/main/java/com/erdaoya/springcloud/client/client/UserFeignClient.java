cad8dde5cb7daec716e5a720850b01a84efcb4c9
zhaozhou <zhaozhou@gomeplus.com>
2017-01-19 11:46:06 +0800

    update feign

--- cloud-service-ext/src/main/java/com/erdaoya/springcloud/client/client/UserFeignClient.java
+++ cloud-service-ext/src/main/java/com/erdaoya/springcloud/client/client/UserFeignClient.java
@@ -1,20 +1,33 @@
 package com.erdaoya.springcloud.client.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 2016/12/25 21:43
+ *
  * @author erdaoya
  */
-@FeignClient(value = "user-service")
+@FeignClient(name = "user-service", fallback = UserFeignClient.HystrixClientFallback.class)
 public interface UserFeignClient {
 
 
     @RequestMapping(method = RequestMethod.GET)
     Object getUser(@RequestParam(value = "id") String id);
 
+    static class HystrixClientFallback implements UserFeignClient {
 
+        @Override
+        public Object getUser(@RequestParam(value = "id") String id) {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("mesages", "Hystrix Fallback");
+            return map;
+        }
+    }
 }
--- cloud-service-ext/src/main/java/com/erdaoya/springcloud/client/client/UserRibbonClient.java
+++ cloud-service-ext/src/main/java/com/erdaoya/springcloud/client/client/UserRibbonClient.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 2017/1/13
  *
@@ -19,12 +22,14 @@ public class UserRibbonClient {
     private RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "getUserFallback")
-    public String getUser(String id) {
-        return restTemplate.getForEntity("http://USER-SERVICE/user/persionalInfo?id=" + id, String.class).getBody();
+    public Object getUser(String id) {
+        return restTemplate.getForEntity("http://USER-SERVICE/user/persionalInfo?id=" + id, Object.class).getBody();
     }
 
-    public String getUserFallback(String id) {
-        return "error";
+    public Object getUserFallback(String id) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("mesages", "Hystrix Fallback");
+        return map;
     }
 
     @HystrixCommand(
