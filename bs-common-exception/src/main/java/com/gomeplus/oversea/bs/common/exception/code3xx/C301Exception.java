61c04852f0bd59c92e590af0375202533e4ff91a
zhaozhou <zhaozhou@gomeplus.com>
2017-02-09 14:10:54 +0800

    add cxxx-exception

---
+++ bs-common-exception/src/main/java/com/gomeplus/oversea/bs/common/exception/code3xx/C301Exception.java
@@ -0,0 +1,22 @@
+package com.gomeplus.oversea.bs.common.exception.code3xx;
+
+
+import com.gomeplus.oversea.bs.common.exception.RESTfull3xxBaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serializable;
+
+/**
+ * 301异常：Moved Permanently：资源或API被迁移到新的URL，API 更名应该使用该code
+ * @author zhaozhou
+ */
+@ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+public class C301Exception extends RESTfull3xxBaseException implements Serializable {
+
+    private static final long serialVersionUID = 2471232079442861956L;
+
+    public C301Exception(String message) {
+        super(message);
+    }
+}
---
+++ bs-common-exception/src/main/java/com/gomeplus/oversea/bs/common/exception/code4xx/C405Exception.java
@@ -0,0 +1,18 @@
+package com.gomeplus.oversea.bs.common.exception.code4xx;
+
+
+
+import com.gomeplus.oversea.bs.common.exception.RESTfull4xxBaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serializable;
+@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+public class C405Exception extends RESTfull4xxBaseException implements Serializable {
+
+	private static final long serialVersionUID = -779530377325082078L;
+
+	public C405Exception(String message) {
+		super(message);
+	}
+}
