19569d9b47e6feafea971a5c9558592ca2b103b2
erdaoya <zz.thoreau@Gmail.com>
2017-03-09 22:28:45 +0800

    test config bus

--- cloud-service-trade/src/main/java/com/erdaoya/springcloud/trade/web/OrderResouce.java
+++ cloud-service-trade/src/main/java/com/erdaoya/springcloud/trade/web/OrderResouce.java
@@ -3,6 +3,8 @@ package com.erdaoya.springcloud.trade.web;
 import com.erdaoya.springcloud.common.exception.code4xx.C404Exception;
 import com.erdaoya.springcloud.trade.entity.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,15 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 @RequestMapping("/trade/order")
 public class OrderResouce {
+
+    @Value("${testvalue.price}")
+    private Double price;
+
     @RequestMapping(method = RequestMethod.GET)
     public Order doGet(@RequestParam Long id) {
         if (id == 1) {
             Order order = new Order();
-            order.setId(1L);
+            order.setId(id);
             order.setCustomerId(1L);
-            order.setPrice(12.32);
+            order.setPrice(price);
             order.setSellerId(2L);
             return order;
         } else {
