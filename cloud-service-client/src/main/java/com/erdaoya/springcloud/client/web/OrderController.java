1b35dc4afd8be95d0e36e77d86a8c4ddde80ecb5
erdaoya <zz.thoreau@Gmail.com>
2017-04-05 00:43:49 +0800

    update zipkin mysql

--- cloud-service-client/src/main/java/com/erdaoya/springcloud/client/web/OrderController.java
+++ cloud-service-client/src/main/java/com/erdaoya/springcloud/client/web/OrderController.java
@@ -23,7 +23,7 @@ public class OrderController {
 
     @RequestMapping(value = "/order", method = RequestMethod.GET)
     public Object getUserByRibbon(String id) {
-        return restTemplate.getForEntity("http://TRADE-SERVICE/trade/order?id=" + id, Object.class).getBody();
+        return restTemplate.getForEntity("http://CLOUD-SERVICE-TRADE/trade/order?id=" + id, Object.class).getBody();
     }
 
 }
