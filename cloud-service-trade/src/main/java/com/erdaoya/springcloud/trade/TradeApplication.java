9ab61230a42b3dea44cb917d9bd36a289b4cd9a6
erdaoya <zz.thoreau@Gmail.com>
2017-07-11 00:34:02 +0800

    upgrade to daslton

--- cloud-service-trade/src/main/java/com/erdaoya/springcloud/trade/TradeApplication.java
+++ cloud-service-trade/src/main/java/com/erdaoya/springcloud/trade/TradeApplication.java
@@ -3,7 +3,6 @@ package com.erdaoya.springcloud.trade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
